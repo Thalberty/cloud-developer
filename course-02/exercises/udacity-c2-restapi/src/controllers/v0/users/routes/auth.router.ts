3354d71fa379c20eff451097b2b34b7356e2b134
Thalbert <thalbert.miranda@gmail.com>
2020-11-05 07:11:07 -0300

    UPDATE - Parse bject user to json

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -24,9 +24,8 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
 }
 
 function generateJWT(user: User): string {
-    return jwt.sign(user, config.jwt.secret
-        );
-}
+    return jwt.sign(user.toJSON(), config.jwt.secret);
+  }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
     // return next();
